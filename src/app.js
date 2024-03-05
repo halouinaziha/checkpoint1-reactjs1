0d0da465db154fa2c49782e88e671337dfaaa88d
halouinaziha <154628090+halouinaziha@users.noreply.github.com>
2024-03-05 11:49:01 +0100

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import './App.css';
+import NavBar from './Components/NavBar/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Forms from './Components/Forms/forms'
+
+function App() {
+  return (
+    <div  className=" Container homepage-bgimage">
+      
+      <NavBar />
+      <Forms></Forms>
+  
+    </div>
+
+
+  );
+}
+
+export default App;
